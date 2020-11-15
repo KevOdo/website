2a24db627c8ac30c3c483522f86fab1a2c64d543
KevOdo <kaod500@york.ac.uk>
2020-11-15 15:28:57 +0000

    added router and template pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
+import "./App.scss"
 import "react-bulma-components/dist/react-bulma-components.min.css"
 
 import Header from "./components/header/header.js"
+import Homepage from "./components/pages/homepage.js"
+import Projects from "./components/pages/projectpage.js"
+import Contact from "./components/pages/contactpage.js"
 
 class App extends Component {
   render() {
     return (
-      <div classname="App">
-        <Header />
-      </div>
+      <Router>
+        <div classname="App">
+          <Header />
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/Projects" component={Projects} />
+          <Route exact path="/Contact" component={Contact} />
+        </div>
+      </Router>
     )
   }
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,22 +1,51 @@
 import React, { Component } from "react"
+import "./header.scss"
 import { nav } from "react-bulma-components"
+import { Link } from "react-router-dom"
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-          <div class="navbar-menu">
-            <a class="navbar-item">Home</a>
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link">More</a>
-              <div class="navbar-dropdown">
-                <a class="navbar-item">About</a>
-                <a class="navbar-item">Jobs</a>
-              </div>
+        <section class="hero hero-image ">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title text">Kevin O'Donnell</h1>
             </div>
           </div>
-        </nav>
+          <div class="hero-foot">
+            <nav
+              class="navbar is-link"
+              role="navigation"
+              aria-label="main navigation"
+            >
+              <div class="navbar-brand">
+                <p
+                  role="button"
+                  class="navbar-burger burger"
+                  aria-label="menu"
+                  aria-expanded="false"
+                  data-target="navbarBasicExample"
+                >
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                </p>
+              </div>
+              <div class="navbar-menu" id="nav-menu">
+                <Link to="/" class="navbar-item text">
+                  Home
+                </Link>
+                <Link to="/Projects" class="navbar-item text">
+                  Projects
+                </Link>
+                <Link to="/Contact" class="navbar-item text">
+                  Contact
+                </Link>
+              </div>
+            </nav>
+          </div>
+        </section>
       </header>
     )
   }
