9c70564eeed6c47cac47d548a6228d171faf4741
KevOdo <kaod500@york.ac.uk>
2020-11-19 01:12:25 +0000

    started working on homepage

--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react"
+import Fade from "react-reveal/Fade"
+
+import "./homepage.scss"
 
 class Homepage extends Component {
   render() {
-    return <div>Hello</div>
+    const intro = (
+      <div className="about has-text-centered">
+        <Fade delay={1000}>
+          <h1 className="title text">Who am I?</h1>
+        </Fade>
+        <Fade delay={2000} duration={1000}>
+          <h2 className="subtitle text">I'm one half of the ModSquad</h2>
+        </Fade>
+      </div>
+    )
+
+    return (
+      <div>
+        <div className="columns main has-text-centered">
+          <div className=" column is-three-fifths is-offset-one-fifth">{intro}</div>
+        </div>
+      </div>
+    )
   }
 }
 
